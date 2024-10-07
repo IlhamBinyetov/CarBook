5e9d43c439dc11b259ef7b55c7cc2cda40eeb4b8
Ilham Binyetov <ilhamgb@code.edu.az>
2024-10-07 10:52:23 +0400

    Create new API Controller(Banners)

---
+++ Presentation/API/Controllers/BannersController.cs
@@ -0,0 +1,41 @@
+﻿using CarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannersController : ControllerBase
+    {
+        private readonly GetBannerQueryHandler _getBannerQueryHandler;
+        private readonly GetBannerByIdQueryHandler _getBannerByIdQueryHandler;
+        private readonly CreateBannerCommandHandler _createBannerCommandHandler;
+        private readonly UpdateBannerCommandHandler _updateBannerCommandHandler;
+        private readonly RemoveBannerCommandHandler _removeBannerCommandHandler;
+
+        public BannersController(GetBannerQueryHandler getBannerQueryHandler, 
+                                GetBannerByIdQueryHandler getBannerByIdQueryHandler, 
+                                CreateBannerCommandHandler createBannerCommandHandler, 
+                                UpdateBannerCommandHandler updateBannerCommandHandler, 
+                                RemoveBannerCommandHandler removeBannerCommandHandler)
+        {
+            _getBannerQueryHandler = getBannerQueryHandler;
+            _getBannerByIdQueryHandler = getBannerByIdQueryHandler;
+            _createBannerCommandHandler = createBannerCommandHandler;
+            _updateBannerCommandHandler = updateBannerCommandHandler;
+            _removeBannerCommandHandler = removeBannerCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BannersList()
+        {
+            var values = await _getBannerQueryHandler.Handle();
+            return Ok(values);
+        }
+
+
+
+
+    }
+}
