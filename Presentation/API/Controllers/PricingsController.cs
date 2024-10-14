82696c92228da4b1417e1ceb6858e1a9e75fb156
Ilham Binyetov <ilhamgb@code.edu.az>
2024-10-14 14:58:12 +0400

    Pricing Handlers and Api Controller created and tested

---
+++ Presentation/API/Controllers/PricingsController.cs
@@ -0,0 +1,55 @@
+﻿using CarBook.Application.Features.Mediator.Commands.PricingCommands;
+using CarBook.Application.Features.Mediator.Queries.PricingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PricingsList()
+        {
+            var values = await _mediator.Send(new GetPricingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPricing(int id)
+        {
+            var value = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("New Pricing added successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing Updated Successfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemovePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok($"Pricing Deleted Successfully. The deleted item id is {id}");
+        }
+    }
+}
