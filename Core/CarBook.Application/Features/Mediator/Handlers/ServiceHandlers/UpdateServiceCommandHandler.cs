46a3b9b6fdc9cbdac604451219f3ae0051580681
Ilham Binyetov <ilhamgb@code.edu.az>
2024-10-14 15:23:58 +0400

    Service Handlers and Api Controller created and tested

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ServiceHandlers/UpdateServiceCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ServiceCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ServiceHandlers
+{
+    public class UpdateServiceCommandHandler : IRequestHandler<UpdateServiceCommand>
+    {
+        private readonly IRepository<Service> _repository;
+
+        public UpdateServiceCommandHandler(IRepository<Service> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateServiceCommand request, CancellationToken cancellationToken)
+        {
+            var updatedItem = await _repository.GetByIdAsync(request.ServiceId);
+           updatedItem.ServiceId = request.ServiceId;
+            updatedItem.Title = request.Title;
+            updatedItem.Description = request.Description;
+            updatedItem.IconUrl = request.IconUrl;
+            await _repository.UpdateAsync(updatedItem);
+        }
+    }
+}
---
+++ Presentation/API/Controllers/ServicesController.cs
@@ -0,0 +1,55 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ServiceCommands;
+using CarBook.Application.Features.Mediator.Queries.ServiceQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ServicesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ServicesList()
+        {
+            var values = await _mediator.Send(new GetServiceQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetService(int id)
+        {
+            var value = await _mediator.Send(new GetServiceByIdQuery(id));
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("New Service added successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateService(UpdateServiceCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Service Updated Successfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveService(int id)
+        {
+            await _mediator.Send(new RemoveServiceCommand(id));
+            return Ok($"Service Deleted Successfully. The deleted item id is {id}");
+        }
+    }
+}
