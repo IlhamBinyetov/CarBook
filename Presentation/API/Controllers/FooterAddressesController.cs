a7e0d2a915c3df18d381ebd36fc9b2545aeb47e9
Ilham Binyetov <ilhamgb@code.edu.az>
2024-10-14 12:33:39 +0400

    Api Controller of FooterAddress  Created with its actions

---
+++ Presentation/API/Controllers/FooterAddressesController.cs
@@ -0,0 +1,57 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddresCommands;
+using CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using CarBook.Application.Features.Mediator.Queries.FooterAddressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FooterAddressesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressesList()
+        {
+            var values = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddress(int id)
+        {
+            var value = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("New FooterAddress Created successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("FooterAddress Updated Successfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFooterAddress(int id)
+        {
+            await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok($"FooterAddress Deleted Successfully. The deleted item id is {id}");
+        }
+    }
+}
