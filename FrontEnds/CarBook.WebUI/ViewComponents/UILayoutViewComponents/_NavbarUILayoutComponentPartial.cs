aa218d9e21e95afc1d8483cb4a7a39720ea4f049
Ilham Binyetov <ilhamgb@code.edu.az>
2024-10-21 18:00:59 +0400

    Create ViewComponents and its partials

---
+++ FrontEnds/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial : ViewComponent
+    {
+     public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
